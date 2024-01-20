dc7600b7f262b6ac2cb254df1638401b25991170
golddash <89572692+golddash@users.noreply.github.com>
2024-01-20 14:38:19 -0800

    hi

--- OldYujafiy/scrape.js
+++ OldYujafiy/scrape.js
@@ -13,6 +13,7 @@ async function scrapeProduct(url)
     console.log(rawText);
     browser.close()
 
+    // test
 }
 
 scrapeProduct('https://www.amazon.com/Fourth-Wing-Empyrean-Rebecca-Yarros/dp/1649374046/ref=zg_d_sccl_2/143-9710279-2256644?pd_rd_w=8wkca&content-id=amzn1.sym.193afb92-0c19-4833-86f8-850b5ba40291&pf_rd_p=193afb92-0c19-4833-86f8-850b5ba40291&pf_rd_r=ZFFZD7JZ11PRF218D9ZX&pd_rd_wg=7Q9P0&pd_rd_r=346499c7-a0b3-4d2d-b4bc-963499f5dbee&pd_rd_i=1649374046&psc=1')
