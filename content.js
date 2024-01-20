7ecd712a037fc526e57aa971c0c7bf7c925d913d
ttoy12 <thomastoy1415@gmail.com>
2024-01-20 12:38:01 -0800

    content.js changes

--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log('Content script received message:', request);
+    console.log('Content.js: Content script received message:', request);
+  
     if (request.action === 'extractH1') {
-      var h1Content = document.querySelector('h1').textContent;
-      chrome.runtime.sendMessage({ action: 'showAlert', h1Content: h1Content });
+        alert("CONTENT.js");
+      var h1Element = document.querySelector('h1');
+  
+      if (h1Element) {
+        var h1Content = h1Element.textContent;
+        chrome.runtime.sendMessage({ action: 'showAlert', h1Content: h1Content });
+      } else {
+        chrome.runtime.sendMessage({ action: 'showAlert', h1Content: 'No <h1> element found' });
+      }
     }
-});
-
-// hello
+  });
