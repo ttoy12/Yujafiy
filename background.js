9682216c0bf527903e6d4efa03683d2f45ab9b87
e94904 <ericlam94904@gmail.com>
2024-01-21 01:28:59 -0800

    nothing broke yet with the json thing

---
+++ background.js
@@ -0,0 +1,37 @@
+// background.js
+var tabIdToDebug; // define tabIdToDebug in the outer scope
+var requestIdToResponse = {};
+
+chrome.tabs.query({url: "https://media.ucsc.edu/*"}, function(tabs) {
+  if (tabs.length > 0) {
+    tabIdToDebug = tabs[0].id; // assign the tab ID of the first tab that matches the URL
+
+    chrome.debugger.attach({tabId: tabIdToDebug}, '1.0', function() {
+      chrome.debugger.sendCommand({tabId: tabIdToDebug}, 'Network.enable', {}, function() {
+        chrome.debugger.onEvent.addListener(allEventHandler);
+      });
+    });
+  }
+});
+
+function allEventHandler(debuggeeId, message, params) {
+  if (tabIdToDebug != debuggeeId.tabId) {
+    return;
+  }
+
+  if (message == 'Network.responseReceived') {
+    if (params.response.url.includes('VideoJSON')) {
+      requestIdToResponse[params.requestId] = params.response;
+    }
+  } else if (message == 'Network.loadingFinished') {
+    if (params.requestId in requestIdToResponse) {
+      chrome.debugger.sendCommand({tabId: tabIdToDebug}, 'Network.getResponseBody', {requestId: params.requestId}, function(response) {
+        if (chrome.runtime.lastError) {
+          console.log(chrome.runtime.lastError.message);
+        } else {
+          console.log('Response body: ', response.body);
+        }
+      });
+    }
+  }
+}
